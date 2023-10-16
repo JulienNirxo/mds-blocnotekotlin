e51577c208b7aa59eca9d1e16ea23a88b968e200
JulienNirxo <millaud.julien@gmail.com>
2023-10-16 17:15:41 +0200

    [INIT] list and page

--- app/src/main/java/com/example/myblocnoteinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/myblocnoteinkotlin/MainActivity.kt
@@ -1,32 +1,23 @@
 package com.example.myblocnoteinkotlin
 
-import Personne
 import android.os.Bundle
+import android.view.View
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.myblocnoteinkotlin.ui.theme.MyBlocNoteInKotlinTheme
 
 class MainActivity : ComponentActivity() {
+    var list = arrayOf("India", "China", "australia", "Portugle", "America", "NewZealand")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            MyBlocNoteInKotlinTheme {
-                // A surface container using the 'background' color from the theme
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                ) {
-                }
-            }
-        }
+        //affichage de mon layout
+        setContentView(R.layout.activity_accueil)
+
+        //initialisation de ma liste
+        var simpleList = findViewById<View>(R.id.simpleListView) as ListView
+        val arrayAdapter: ArrayAdapter<String> =
+            ArrayAdapter<String>(this, R.layout.item_list, R.id.textView, list)
+        simpleList.setAdapter(arrayAdapter)
     }
 }
+
