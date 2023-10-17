276ed7ceca71d584f8661471dcd74960a8269834
JulienNirxo <millaud.julien@gmail.com>
2023-10-17 10:56:41 +0200

    [FEAT] V1 list

--- app/src/main/java/com/example/myblocnoteinkotlin/MainActivity.kt
+++ app/src/main/java/com/example/myblocnoteinkotlin/MainActivity.kt
@@ -9,21 +9,38 @@ import android.widget.ListView
 import androidx.activity.ComponentActivity
 
 class MainActivity : ComponentActivity() {
-    var list = arrayOf("India", "China", "australia", "Portugle", "America", "NewZealand")
+    var list = ArrayList<Any>()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //affichage de mon layout
         setContentView(R.layout.activity_accueil)
 
+        //récupération de la liste
+        val intent = getIntent()
+        val titre = intent.getStringExtra("titre")
+        val note = intent.getStringExtra("note")
+        val listCache = intent.getStringArrayListExtra("list")
+        if (listCache != null) {
+            list.addAll(listCache)
+        }
+
+        //si on ajoute une note, on ajoute à la liste
+        if(titre.toString() != "" || titre.toString() != null){
+            list.add(arrayListOf(titre.toString(), note.toString()))
+        }
+
+
         //initialisation de ma liste
         var simpleList = findViewById<View>(R.id.simpleListView) as ListView
-        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter<String>(this, R.layout.item_list, R.id.textView, list)
+        val arrayAdapter: ArrayAdapter<Any> = ArrayAdapter<Any>(this, R.layout.item_list, R.id.textView, list)
         simpleList.setAdapter(arrayAdapter)
 
         //initialisation de mon bouton ajout de note
         val btnAddNote = findViewById<Button>(R.id.BtnAddNote)
         btnAddNote.setOnClickListener {
             val intent = Intent(this, add_note::class.java)
+            val stringList = list.map { it.toString() }.toMutableList()
+            intent.putStringArrayListExtra("list", ArrayList(stringList))
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/myblocnoteinkotlin/add_note.kt
+++ app/src/main/java/com/example/myblocnoteinkotlin/add_note.kt
@@ -1,11 +1,34 @@
 package com.example.myblocnoteinkotlin
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
 class add_note : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_note)
+
+        //initialisation des composants
+        val btnAddNote = findViewById<Button>(R.id.btnAdd)
+        val EditTextTitre = findViewById<EditText>(R.id.EditTextTitre)
+        val EditTextNote = findViewById<EditText>(R.id.EditTextNote)
+
+        //ajout d'une note
+        btnAddNote.setOnClickListener {
+            val editTextValueTitre: String = EditTextTitre.getText().toString()
+            val editTextValueNote: String = EditTextNote.getText().toString()
+
+            val intentAccueil = getIntent()
+            val list = intent.getStringArrayListExtra("list")
+
+            val intent = Intent(applicationContext, MainActivity::class.java);
+            intent.putExtra("titre", editTextValueTitre)
+            intent.putExtra("note", editTextValueNote)
+            intent.putStringArrayListExtra("list", list)
+            startActivity(intent);
+        }
     }
 }
